c00936e4fedcf4ff021e748191ae3952d9bdaadc
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-05-28 12:09:41 +0600

    added file after complete header

---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,52 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+
+const Navbar = () => {
+    const [user] = useAuthState(auth);
+
+    // const logout = () => {
+    //     signOut(auth);
+    // };
+
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/reviews'>Reviews</Link></li>
+        <li><Link to='/contactUs'>Contact Us</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        {/* {
+            user && <li><Link to='/dashboard'>Dashboard</Link></li>
+        }
+        <li>{user ? <button className="btn btn-ghost" onClick={logout}>Sign out</button> : <Link to='/login'>Login</Link>}</li> */}
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl ml-10">Zara Car Manufacturer House</a>
+            </div>
+            <div className="navbar-center hidden lg:flex ml-36">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navber-end">
+                <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
