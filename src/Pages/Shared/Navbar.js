1db15b608f02db31685823bc99acb0054fbb21f6
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-10-18 21:32:49 +0600

    added after updated

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -56,7 +56,24 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" className="menu menu-compact lg:hidden dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {menuItems}
+                        <li className='font-bold'><Link to='/'>Home</Link></li>
+
+                        <div className="dropdown dropdown-hover cursor-pointer dropdown-container pt-3 font-bold">
+                            <label className='pl-4 lg:pr-3' tabIndex="0">Pages</label>
+                        </div>
+                        <li className='font-bold'><Link to='/blogs'>Blogs</Link></li>
+                        <li className='font-bold'><Link to='/review'>Reviews</Link></li>
+                        <li className='font-bold'><Link to='/myportfolio'>My Portfolio</Link></li>
+                        {
+                            user && <>
+                                <li className='font-bold'><Link to='/dashboard'>Dashboard</Link></li>
+                                <li className='font-bold'><Link to='/manageparts'>Parts</Link></li>
+                                {admin && <li className='font-bold'><Link to='/addproduct'>Add Product</Link></li>}
+                            </>
+                        }
+                        {
+                            <li className='font-bold'>{user ? <button className="font-bold" onClick={logout}>Signout</button> : <Link to='/login'>Login</Link>}</li>}
+                        <li className='font-bold'><Link to='/contactUs'>Contact Us</Link></li>
                     </ul>
                 </div>
             </div>
